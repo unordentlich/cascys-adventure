c4086819e032d90334cde3328d69d430f0bbc648
Jonas <jk06gaming@gmail.com>
2024-07-16 00:26:59 +0200

    Added game intro (timesaver for preloads)

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
-const { BrowserWindow, app } = require("electron")
+const { BrowserWindow, app, ipcMain } = require("electron")
+const path = require('node:path')
 
 const GAME_NAME = "Cascy's Coding Adventure"
 let win;
 
-const createWindow = () => {
+const showIntro = () => {
+    win = new BrowserWindow({
+        width: 800,
+        height: 900,
+        minHeight: 700,
+        minWidth: 1200,
+        icon: 'icon.png',
+        title: GAME_NAME,
+        titleBarStyle: 'hidden',
+        titleBarOverlay: {
+            color: '#000',
+            symbolColor: '#fff',
+            height: 32
+        },
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        },
+    });
+
+    win.loadFile("views/intro/logo.html");
+}
+
+function leaveIntro(event) {
+    win.loadFile("views/main_menu.html");
+    setTimeout(() => {
+        win.setTitleBarOverlay({
+            color: '#3e3b48',
+            symbolColor: '#fff',
+            height: 32
+        });
+    }, 2000)
+}
+
+const createMainWindow = () => {
     win = new BrowserWindow({
         width: 800,
         height: 900,
@@ -19,18 +53,17 @@ const createWindow = () => {
         },
     });
 
-    win.webContents.openDevTools();
-
     win.removeMenu();
 
     win.loadFile("views/main_menu.html")
-    //attachTitlebarToWindow(win);
 }
 
 app.whenReady().then(() => {
-    createWindow()
+    showIntro();
+    //win.webContents.openDevTools();
+    ipcMain.on('leave-intro', leaveIntro);
 });
 
 try {
     require('electron-reloader')(module)
-  } catch (_) {}
+} catch (_) { }
