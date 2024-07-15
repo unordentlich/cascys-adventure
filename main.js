0c9dba43931018a6e95fa555cd0d3726028ab7e9
Jonas <jk06gaming@gmail.com>
2024-07-15 23:16:40 +0200

    added main menu and first graphics

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-console.log('Hello from Electron 👋')
+const { BrowserWindow, app } = require("electron")
+
+const GAME_NAME = "Cascy's Coding Adventure"
+let win;
+
+const createWindow = () => {
+    win = new BrowserWindow({
+        width: 800,
+        height: 900,
+        minHeight: 700,
+        minWidth: 1200,
+        icon: 'icon.png',
+        title: GAME_NAME,
+        titleBarStyle: 'hidden',
+        titleBarOverlay: {
+            color: '#3e3b48',
+            symbolColor: '#fff',
+            height: 32
+        },
+    });
+
+    win.webContents.openDevTools();
+
+    win.removeMenu();
+
+    win.loadFile("views/main_menu.html")
+    //attachTitlebarToWindow(win);
+}
+
+app.whenReady().then(() => {
+    createWindow()
+});
+
+try {
+    require('electron-reloader')(module)
+  } catch (_) {}
