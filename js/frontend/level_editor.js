81ad3fa0066579ec4158bbc5f4400c34f4c7a9a1
Jonas <jk06gaming@gmail.com>
2024-07-16 21:45:23 +0200

    Added mouse movement again

--- js/frontend/level_editor.js
+++ js/frontend/level_editor.js
@@ -8,6 +8,11 @@ let ctx;
 let offsetX = 0;
 let offsetY = 0;
 
+let dragging = false;
+let mouseClicked = [];
+let mouseOffsetX = 0;
+let mouseOffsetY = 0;
+
 let zoom = 1;
 let zoomStep = 0.2;
 let maxZoom = 4.5;
@@ -44,6 +49,24 @@ document.addEventListener("DOMContentLoaded", () => {
             zoom -= zoomStep;
         }
         prepareCanvas();
+    });
+
+    canvas.addEventListener("mousedown", (e) => {
+        dragging = true;
+        mouseClicked = [e.clientX, e.clientY];
+    });
+
+    canvas.addEventListener("mousemove", (e) => {
+        if(!dragging) return;
+        mouseOffsetX = e.clientX - mouseClicked[0];
+        mouseOffsetY = e.clientY - mouseClicked[1];
+        console.log(mouseOffsetX, mouseOffsetY);
+        prepareCanvas();
+    });
+
+    canvas.addEventListener("mouseup", () => {
+        if(!dragging) return;
+        dragging = false;
     })
 });
 
@@ -67,10 +90,13 @@ document.addEventListener('keydown', (e) => {
 });
 
 function prepareCanvas() {
+    console.log(mouseOffsetX, mouseOffsetY);
+    ctx.restore();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.save();
     ctx.resetTransform();
     ctx.scale(zoom, zoom);
+    ctx.translate(mouseOffsetX, mouseOffsetY);
 
     let xPos = 0 + (offsetX * pixelSize);
     let yPos = 0 + (offsetY * pixelSize);
