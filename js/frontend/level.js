650d741585c5d57ab87b780f6cd5bfa542079a92
Jonas <jk06gaming@gmail.com>
2024-07-28 21:30:26 +0200

    start of border movement concept

--- js/frontend/level.js
+++ js/frontend/level.js
@@ -1,34 +1,36 @@
 var level = {};
-var x = 0, y = 0, stepSize = 5;
+var x = 0, y = 0, stepSize = 7, viewBorder = 50;
+let player;
 
 document.addEventListener('DOMContentLoaded', () => {
     loadLevel();
-});
+    player = document.getElementById('player');
 
-const movementKeys = ['w', 'a', 's', 'd', 'ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
-document.addEventListener('keydown', (e) => {
-    console.log(e.key);
-    if (!movementKeys.includes(e.key)) return;
-    if (e.key === 'w' || e.key === 'ArrowUp') {
-        y -= 1;
-    } else if (e.key === 's' || e.key === 'ArrowDown') {
-        y += 1;
-    } else if (e.key === 'd' || e.key === 'ArrowRight') {
-        x += 1;
-    } else if (e.key === 'a' || e.key === 'ArrowLeft') {
-        x -= 1;
-    }
+    const movementKeys = ['w', 'a', 's', 'd', 'ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+    document.addEventListener('keydown', (e) => {
+        if (!movementKeys.includes(e.key)) return;
+        if (e.key === 'w' || e.key === 'ArrowUp') {
+            y -= 1;
+        } else if (e.key === 's' || e.key === 'ArrowDown') {
+            y += 1;
+        } else if (e.key === 'd' || e.key === 'ArrowRight') {
+            x += 1;
+        } else if (e.key === 'a' || e.key === 'ArrowLeft') {
+            x -= 1;
+        }
 
-    let player = document.getElementById('player');
-    player.style.transform = `translate(${x * stepSize}px, ${y * stepSize}px)`;
-    player.style.transition = 'transform 0.2s linear';
-    player.setAttribute('state', 'moving');
-});
+        player.style.transform = `translate(${x * stepSize}px, ${y * stepSize}px)`;
+        player.style.transition = 'transform 0.2s linear';
+        player.setAttribute('state', 'moving');
 
-document.addEventListener('keyup', (e) => {
-    if (!movementKeys.includes(e.key)) return;
-    let player = document.getElementById('player');
-    player.setAttribute('state', 'idle');
+        checkBorders();
+    });
+
+    document.addEventListener('keyup', (e) => {
+        if (!movementKeys.includes(e.key)) return;
+        let player = document.getElementById('player');
+        player.setAttribute('state', 'idle');
+    });
 });
 
 function loadLevel() {
@@ -42,9 +44,9 @@ function loadLevel() {
 
         var img = new Image();
         console.log(`levels/${level.file}.png`);
+        img.id = 'map-bg';
         window.electronAPI.loadPath(`levels/${level.file}.png`).then((p) => {
-            console.log(p);
-            img.src = p;    
+            img.src = p;
         });
 
         map.appendChild(img);
@@ -54,4 +56,36 @@ function loadLevel() {
 
         document.querySelector('.loading-screen').style.display = 'none';
     });
+}
+
+function checkBorders() {
+    var location = player.getBoundingClientRect();
+    var viewArea = document.getElementById('map').getBoundingClientRect();
+    var map = document.getElementById('map-bg').getBoundingClientRect();
+
+    console.log(map.x, map.y)
+    if (location.x < viewBorder) {
+        console.log('border hit');
+        scroll('x', -1);
+    } else if(location.x + location.width > viewArea.width - viewBorder) {
+        console.log('border right hit');
+        scroll('x', 1);
+    } else if (location.y < viewBorder) {
+        console.log('border hit');
+    } else if(location.y + location.height > viewArea.height - viewBorder) {
+        console.log('border hit');
+    }
+
+    function scroll(axis = 'x', direction = 1) {
+        if(axis === 'x') {
+            if((direction === 1 && map.x - stepSize > 0) || (direction === -1 && map.x + map.width < viewArea.width)) {
+                document.getElementById('map-bg').style.transform = `translate(${viewArea.width - map.width}px, ${map.y}px)`;
+                //todo
+            } else {
+                document.getElementById('map-bg').style.transform = `translate(${direction === 1 ? map.x - map.width : map.x + map.width}px, ${map.y}px)`;
+                x = 0;
+                player.style.transform = `translate(${x * stepSize}px, ${y * stepSize}px)`;
+            }
+        }
+    }
 }
