a36affb0f967af9e515cca726b12d148b662a00e
Jonas <jk06gaming@gmail.com>
2024-07-30 22:43:06 +0200

    omg yeah that looks better now

--- js/frontend/level.js
+++ js/frontend/level.js
@@ -13,24 +13,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
         var direction = 0;
         if (e.key === 'w' || e.key === 'ArrowUp') {
-            y -= stepSize;
             direction = 1;
         } else if (e.key === 's' || e.key === 'ArrowDown') {
-            y += stepSize;
             direction = -1;
         } else if (e.key === 'd' || e.key === 'ArrowRight') {
-            x += stepSize;
             direction = 1;
         } else if (e.key === 'a' || e.key === 'ArrowLeft') {
-            x -= stepSize;
             direction = -1;
         }
 
-        player.style.transform = `translate(${x}px, ${y}px)`;
-        player.style.transition = 'transform 0.2s linear';
+        var borderReached = checkBorders(direction);
+
+        console.log(`x: ${x}, y: ${y}`);
+        if (!borderReached) {
+            if(e.key === 'w' || e.key === 'ArrowUp') y -= stepSize;
+            if(e.key === 'a' || e.key === 'ArrowLeft') x -= stepSize;
+            if(e.key === 's' || e.key === 'ArrowDown') y += stepSize;
+            if(e.key === 'd' || e.key === 'ArrowRight') x += stepSize;
+
+            player.style.transform = `translate(${x}px, ${y}px)`;
+            player.style.transition = 'transform 0.2s linear';
+        }
         player.setAttribute('state', 'moving');
 
-        checkBorders(direction);
     });
 
     document.addEventListener('keyup', (e) => {
@@ -70,17 +75,22 @@ function checkBorders(direction) {
     var viewArea = document.getElementById('map').getBoundingClientRect();
     var map = document.getElementById('map-bg').getBoundingClientRect();
 
-    scroll(direction);
+    const MapToPlayerStepFactor = map.width / viewArea.width;
 
-    function scroll(direction = 0) {
-        var m = document.getElementById('map-bg');
-        const mapToPlayerRatio = map.width / viewArea.width;
+    if (playerLoc.x < viewArea.width / 2) return false; // The screen should only move if the player is in the middle of the screen
+    if (nearInt(map.x * -1 + viewArea.width, map.width, 5)) return false; // The screen should only move if the map is not at the end
 
-        mapX += (direction === 1 ? stepSize : -stepSize) * mapToPlayerRatio;
-        mapY += stepSize * 2; // do same for height
-        console.log(mapX);
-
-        m.style.transform = `translate(-${mapX}px, -${0}px)`;
-        m.style.transition = 'transform 0.2s linear';
+    //move the map in the same direction with the same speed as the player
+    if (direction === 1) {
+        mapX -= stepSize * MapToPlayerStepFactor;
+    } else if (direction === -1) {
+        mapX += stepSize * MapToPlayerStepFactor;
     }
+
+    document.getElementById('map-bg').style.transform = `translate(${mapX}px, ${mapY}px)`;
+    return true;
+}
+
+function nearInt(op, target, range) {
+    return op < target + range && op > target - range;
 }
